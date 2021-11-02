d00b7a2607096c39417716445cd2ec9a667603b8
Gastonx <56830127+Gastonx@users.noreply.github.com>
2021-11-02 16:31:46 +0100

    BulletShooter

---
+++ Assets/Script/Hugo/BulletShooter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    public float TimerA;
+    public float TimerB;
+    private float _Timer;
+    public float TimetoDodge;
+    
+ 
+
+    public bool Sheltered = false;
+
+    public void Start()
+    {
+        StartCoroutine(Shooting());
+    }
+
+    
+
+
+    IEnumerator Shooting()
+    {
+        _Timer = Random.Range(TimerA, TimerB);
+        yield return new WaitForSeconds(_Timer);
+        
+
+        StartCoroutine(Dodge());
+
+    }
+
+    IEnumerator Dodge()
+    {
+        //Le temps avant que le dodge time se finisse
+        yield return new WaitForSeconds(TimetoDodge);
+       
+        Shooted();
+        StartCoroutine(Shooting());
+    }
+
+   
+
+
+    public void Sheltering()
+    {
+        
+        StopCoroutine(Dodge());
+        StartCoroutine(Shooting());
+    }
+
+    public void Shooted()
+    {
+        Debug.Log("shooted");
+    }
+
+
+}
