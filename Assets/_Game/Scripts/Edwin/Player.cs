80a00a627c0b76568beba17431065db56f522673
edwin-od <edwin.odeimi@lau.edu>
2021-11-04 10:08:15 +0100

    sdfisjf

---
+++ Assets/_Game/Scripts/Edwin/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            Debug.Log("HIT");
+            EventManager.instance.OnLostOneLife.Invoke();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))    // le levier et baissé
+        {
+            Debug.Log(KeyCode.E);
+        }
+    }
+}
