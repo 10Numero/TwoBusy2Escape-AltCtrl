2353408bd40ca12866236a465bd93e55d40d59cd
Hugo <hugo.tuil@edu.devinci.fr>
2021-11-05 00:11:47 +0100

    Menu, Win, Lose scenes

---
+++ Assets/_Game/Scripts/Hugo T/ReturnMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnMenu : MonoBehaviour
+{
+    public float timeToReturn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ReturnToMenu());
+    }
+
+    IEnumerator ReturnToMenu()
+    {
+        yield return new WaitForSeconds(timeToReturn);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
