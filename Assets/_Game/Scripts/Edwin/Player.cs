f03973f9e3ccf8f7145fffdb12e8e2b3bd308218
edwin-od <edwin.odeimi@lau.edu>
2021-11-05 00:33:25 +0100

    Obstacle1

--- Assets/_Game/Scripts/Edwin/Player.cs
+++ Assets/_Game/Scripts/Edwin/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     {
         if (other.tag == "Obstacle")
         {
-            Destroy(other.gameObject);
+            Destroy(other.transform.parent.gameObject);
             EventManager.instance.OnLostOneLife.Invoke();
         }
     }
