f55fcfa8dc8eda636cc3ed71b4d709a7b33eda3b
FlorianGOZARD <florian.gozard@wearexone.com>
2022-03-16 11:42:17 +0100

    multi display change with plus or minus

--- Assets/_Game/Scripts/Flo/MultipleScreensDisplay.cs
+++ Assets/_Game/Scripts/Flo/MultipleScreensDisplay.cs
@@ -4,11 +4,11 @@ public class MultipleScreensDisplay : MonoBehaviour
 {
     public Camera[] cameras;
     private int lastDisplayLength;
+    private int currentDisplayLength;
 
     void Awake()
     {
-        for (int i = 1; i < Display.displays.Length; i++)
-            Display.displays[i].Activate();
+        currentDisplayLength = 1;
     }
 
     private void Start()
@@ -29,16 +29,53 @@ public class MultipleScreensDisplay : MonoBehaviour
             else
                 cameras[i].enabled = false;
         }
-    }
 
-    private void LateUpdate()
-    {
-        if (lastDisplayLength != Display.displays.Length)
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
         {
-            EventManager.instance.OnDisplaysLengthChanged.Invoke(Display.displays.Length);
+            currentDisplayLength++;
 
-            for (int i = 1; i < Display.displays.Length; i++)
+            if (Display.displays.Length > currentDisplayLength)
+            {
+                Debug.Log("Current : " + currentDisplayLength);
+                for (int i = 1; i < currentDisplayLength; i++)
+                {
+                    Display.displays[i].Activate();
+                }
+            }
+            else
+            {
+                currentDisplayLength--;
+                Debug.Log("Tried to add screen.");
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            currentDisplayLength--;
+            
+            if (currentDisplayLength <= 0)
+            {
+                Debug.Log("Display can't be equal to 0. Set to 1.");
+                currentDisplayLength = 1;
+            }
+
+
+            for (int i = 1; i < currentDisplayLength; i++)
+            {
                 Display.displays[i].Activate();
+            }
+ 
         }
     }
+
+    // private void LateUpdate()
+    // {
+    //     if (lastDisplayLength != Display.displays.Length)
+    //     {
+    //         EventManager.instance.OnDisplaysLengthChanged.Invoke(Display.displays.Length);
+    //
+    //         for (int i = 1; i < Display.displays.Length; i++)
+    //             Display.displays[i].Activate();
+    //     }
+    // }
 }
